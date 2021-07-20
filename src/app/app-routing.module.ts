af8883a1ae1abe1b52ae416701353258aa678a49
Yashika Garg <yashikagarg13@gmail.com>
2021-07-20 11:24:02 +0530

    Setup HTTPClient  and angular material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { StoryListComponent } from './story-list/story-list.component';
+import { StoryDetailsComponent } from './story-details/story-details.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: 'stories', pathMatch: 'full'},
+  { path: 'stories', component: StoryListComponent },
+  { path: 'details', component: StoryDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+import { StoryListComponent } from './story-list/story-list.component';
+import { StoryDetailsComponent } from './story-details/story-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StoryListComponent,
+    StoryDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule,
+    MatProgressSpinnerModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
